2092105ed20922d5e679edc46f69917f5af379a2
Jorge Vargas <jorgevargasla@gmail.com>
2020-05-27 22:09:43 -0500

    solve sum all

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+  if (min < 0 || max < 0 || typeof min !== "number" || typeof max !== "number") return "ERROR";
 
+  if (min > max) {
+    max = [min, min = max][0];
+  }
+
+  // const sum = max + min;
+  // const substract = max - min;
+  // const subByTwo = substract / 2;
+  // const isInteger = subByTwo % 1 == 0;
+  // const ceilSubByTwo = Math.ceil(subByTwo);
+
+  // let result = sum * ceilSubByTwo;
+  // result += isInteger ? min + subByTwo : 0;
+
+  // return result;
+  return (min + max) * Math.ceil((max - min) / 2) + (((max - min) / 2) % 1 == 0 ? min + ((max - min) / 2) : 0);
 }
 
 module.exports = sumAll
