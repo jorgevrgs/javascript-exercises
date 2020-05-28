f66aeb340c497d0648fcd26e4641e9d43aa64194
Jorge Vargas <jorgevargasla@gmail.com>
2020-05-28 17:40:28 -0500

    solve palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function() {
+const palindromes = function(word) {
+    clean = word.toLowerCase().replace(/[\s\W]+/g, '');
 
+    return clean.split('').reverse().join('') === clean;
 }
 
 module.exports = palindromes
