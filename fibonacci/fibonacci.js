4357a8f30a5d31f62c0abf7bd9e1f6c3766a3327
Jorge Vargas <jorgevargasla@gmail.com>
2020-05-29 09:46:03 -0500

    resolve fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
 
+    if (number < 0) return 'OOPS';
+    if (number == 0) return 0;
+    let sum = [0, 1];
+    for (i = 1; i < number; i++) {
+        sum = [sum[sum.length - 1], sum[sum.length - 1] + sum[sum.length - 2]];
+    }
+
+    return sum[sum.length - 1];
 }
 
 module.exports = fibonacci
