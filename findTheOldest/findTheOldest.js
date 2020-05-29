b81f7c1bc2f75aa3a01d9c596cf4929b38cbcce2
Jorge Vargas <jorgevargasla@gmail.com>
2020-05-29 14:26:28 -0500

    resolve find the oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        person.age = (person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear()) - person.yearOfBirth;
+        return oldest.age > person.age ? oldest : person;
+    }, people[0]);
 }
 
 module.exports = findTheOldest
